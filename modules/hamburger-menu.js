b49282b5191d838cfccb790bc38f1ce4f1853eed
BeEmpathic <xdgrzybeknr1xd@gmail.com>
2025-04-07 18:07:47 +0200

    Changed blackout position so It always covers the whole user's screen

--- modules/hamburger-menu.js
+++ modules/hamburger-menu.js
@@ -6,9 +6,9 @@ const foldersNavbar = document.querySelector("[data-folders-navbar]")
 const blackout = document.createElement("div")
 blackout.style.backgroundColor = "RGBA(0, 0, 0, 0.8)"
 blackout.style.zIndex = 3
-blackout.style.position = "absolute"
-blackout.style.width = "100dvw"
-blackout.style.height = "100dvh"
+blackout.style.position = "fixed"
+blackout.style.minWidth = "100dvw"
+blackout.style.minHeight = "100dvh"
 blackout.style.top = 0
 blackout.style.right = 0
 
