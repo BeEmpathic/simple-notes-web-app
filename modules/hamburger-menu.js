6b3448604afcabc7b8e6bfaf1f22cfbccad6ad35
BeEmpathic <xdgrzybeknr1xd@gmail.com>
2025-04-07 17:46:45 +0200

    Made hamburger's menu functionality in js added blackout when you trigger the hamburger menu

--- modules/hamburger-menu.js
+++ modules/hamburger-menu.js
@@ -1,11 +1,47 @@
+
 const hamburgerMenuCheckbox = document.querySelector("[data-hamburger-menu-checkbox]")
 const foldersNavbar = document.querySelector("[data-folders-navbar]")
-hamburgerMenuCheckbox.addEventListener("click", () => {
-    console.log("Hamburger been clicked")
-    foldersNavbar.classList.toggle("active")
+
+const blackout = document.createElement("div")
+blackout.style.backgroundColor = "RGBA(0, 0, 0, 0.8)"
+blackout.style.zIndex = 3
+blackout.style.position = "absolute"
+blackout.style.width = "100dvw"
+blackout.style.height = "100dvh"
+blackout.style.top = 0
+blackout.style.right = 0
+
+
+function handleHamburgerMenu(e) {
+    if (hamburgerMenuCheckbox.checked) {
+        foldersNavbar.classList.add("active")
+        document.body.appendChild(blackout)
+    } else {
+        foldersNavbar.classList.remove("active")
+        document.body.removeChild(blackout)
+    }
+
+
+}
+
+blackout.addEventListener("click", () => {
+    foldersNavbar.classList.remove("active")
+    document.body.removeChild(blackout)
+    hamburgerMenuCheckbox.checked = false
 })
 
-foldersNavbar.addEventListener("blur", () => {
-    console.log("blur was fired")
-    foldersNavbar.classList.toggle("active")
-})
+hamburgerMenuCheckbox.addEventListener("click", handleHamburgerMenu)
+
+
+
+// document.addEventListener("click", (e) => {
+
+//     if (hamburgerMenuCheckbox.checked && e.target.hasOwnProperty("folders-navbar")) {
+//         foldersNavbar.classList.remove("active")
+//         console.log("The document event worked")
+//         e.stopPropagation()
+//     }
+// })
+
+
+
