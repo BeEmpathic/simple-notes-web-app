a84c8480c4803629923255733473fa499e305724
BeEmpathic <xdgrzybeknr1xd@gmail.com>
2025-04-08 10:24:36 +0200

    Added a script checking if the website changd, then reseting the chache so user can see the new view of the website

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 import { displayNotes, editNote } from "./modules/notes.js"
 import "./modules/hamburger-menu.js"
+
+// Check if a new cache is available on page load.
+window.applicationCache.addEventListener('updateready', function (e) {
+    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+        // Browser downloaded a new app cache.
+        // Swap it in and reload the page to get the new hotness.
+        window.applicationCache.swapCache();
+        if (confirm('A new version of this site is available. Load it?')) {
+            window.location.reload();
+        }
+    } else {
+        // Manifest didn't changed. Nothing new to server.
+    }
+}, false);
+
+
 // deal with note overflow so you can just scroll it seems easy to do
 
 displayNotes()
